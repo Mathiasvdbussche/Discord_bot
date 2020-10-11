52e1cab2db4e670d48613ed73ffd2a9350d28beb
Mathiasvdbussche <mathiasvandenbussche@hotmail.com>
2020-10-11 16:09:51 +0200

    8ball added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 from discord.ext import commands
 
@@ -10,16 +11,42 @@ async def on_ready():
 
 @client.command()
 async def ping(ctf):
-    await ctf.send('bong!')
+    await ctf.send(f'{round(client.latency * 1000)}ms')
 
-@client.event
-async def on_member_join(member):
-    print("Welkom homo " + member)
+@client.command()
+async def hi(hi):
+    await hi.send("hello there!")
 
-@client.event
-async def on_member_remove(member):
-    print(f' {member} has left the server.')
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                'It is decidedly so.',
+                'Without a doubt.',
+                'Yes – definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'Outlook good.',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                "Don't count on it.",
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
+#@client.event
+#async def on_member_join(member):
+#   print("Welkom homo " + member)
+
+#@client.event
+#async def on_member_remove(member):
+#    print(f' {member} has left the server.')
 
 client.run('')
-
