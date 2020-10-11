68059826b2b6e10434c34f6ceda72b5b03cd3fa5
mathiasvdbussche <mathiasvandenbussche@hotmail.com>
2020-10-11 13:02:02 +0200

    foutje eruit gehaald

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#en nu voor echt zegt
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix=".")
+
+@client.event
+async def on_ready():
+    print("bot is ready.")
+
+client.run('NzY0Nzk4NjM3NjE1NDgwODQy.X4LgPA.mWK2rfXHZfNTECa1BK934JkgDpQ')
