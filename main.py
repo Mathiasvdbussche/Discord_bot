93d909b040b9b05c176844f6a9cdd62cd053c15b
LucasG-252 <lucasgellens@outlook.com>
2020-10-11 13:58:01 +0200

    d6

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix=".")
@@ -9,6 +10,10 @@ async def on_ready():
 
 @client.command()
 async def ping(ctf):
-    await ctf.send('bong!')    
+    await ctf.send('bong!')
+
+@client.command()
+async def roll(d6):
+    await d6.send(random(1,6))
 
 client.run('')
