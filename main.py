1336a68f96644c815229a2204c55c5c0657ac03e
LucasG-252 <lucasgellens@outlook.com>
2020-10-11 23:32:55 +0200

    fixed ex. d10 roll command

--- main.py
+++ main.py
@@ -42,34 +42,52 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 @client.command()
-async def d6(ctx):
-    options = [1,2,3,4,5,6]
-    await ctx.send('You rolled: ' + str(random.choice(options)))
+async def roll(ctx, *, rollType):
+    rollType = rollType.lower()
+    getal = rollType[0: rollType.index('d')] + '1'        
+    dice = rollType[rollType.index('d'):]
+    teller = 1
+    result = 0
+    stringResult = ''
+    tempResult = 0
+    
+    if int(getal) > 1:
+        getal = int(getal) - 1
 
-@client.command()
-async def d8(ctx):
-    options = [1,2,3,4,5,6,7,8]
-    await ctx.send('You rolled: ' + str(random.choice(options)))
+    while teller <= int(getal):
+        
+        if dice == 'd4':
+            tempResult = random.randint(1,4)
+            result += tempResult                        
+        elif dice == 'd6':
+            tempResult = random.randint(1,6)
+            result += tempResult
+        elif dice == 'd8':
+            tempResult = random.randint(1,8)
+            result += tempResult
+        elif dice == 'd10':
+            tempResult = random.randint(1,10)
+            result += tempResult
+        elif dice == 'd100':
+            tempResult = random.randint(1,10)*10
+            result += tempResult
+        elif dice == 'd12':
+            tempResult = random.randint(1,12)
+            result += tempResult
+        elif dice == 'd20':
+            tempResult = random.randint(1,20)
+            result += tempResult
+        else:
+            result = 'Invalid dice specified!!!'
+        
+        if teller == int(getal):
+            stringResult += str(tempResult) +' ==> dice: '
+        else:
+            stringResult += str(tempResult) +' + '
+        
+        teller += 1        
+    await ctx.send('You rolled: ' + str(result)+ '\n' + '(' + stringResult + dice + ')')
 
-@client.command()
-async def d10(ctx):
-    options = [1,2,3,4,5,6,7,8,9,10]
-    await ctx.send('You rolled: ' + str(random.choice(options)))
-
-@client.command()
-async def d100(ctx):
-    options = [1,2,3,4,5,6,7,8,9,10]
-    await ctx.send('You rolled: ' + str((random.choice(options)*10) + random.choice(options)))
-
-@client.command()
-async def d12(ctx):
-    options = [1,2,3,4,5,6,7,8,9,10,11,12]
-    await ctx.send('You rolled: ' + str(random.choice(options)))
-
-@client.command()
-async def d20(ctx):
-    options = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    await ctx.send('You rolled: ' + str(random.choice(options)))
 #@client.event
 #async def on_member_join(member):
 #   print("Welkom homo " + member)
@@ -78,4 +96,4 @@ async def d20(ctx):
 #async def on_member_remove(member):
 #    print(f' {member} has left the server.')
 
-client.run('')
+client.run('NzY0Nzk4NjM3NjE1NDgwODQy.X4LgPA.ztzmLHBaRf9wdcyI7tcRqFWMt54')
